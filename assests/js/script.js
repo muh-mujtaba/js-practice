ec219ecccfcfb23fb67aeff49248e8c082e6dc85
M.Mujtaba <muhammadmujtaba0978@gmail.com>
2022-05-17 18:09:16 +0500

    *

--- assests/js/script.js
+++ assests/js/script.js
@@ -20,26 +20,32 @@
 //         console.log("you are eligible");
 //     }
 // }
-let x = prompt("Enter your Table Number");
+// let x = prompt("Enter your Table Number");
 // document.write("Table of " + x + " is given"+"<br>");
 // for(let a=1; 11>a; a++){
 //     // console.log(a*x);
 //     document.write(x + "*" + a + "=" + a*x + "<br>");
 // }
-for(let a=1; 11>a; a++){
-    var tr = document.createElement('tr');
-    var td1 = tr.appendChild(document.createElement('td'));
-    var td2 = tr.appendChild(document.createElement('td'));
-    var td3 = tr.appendChild(document.createElement('td'));
-    td1.innerHTML = x;
-    td2.innerHTML = a;
-    td3.innerHTML = a*x;
-    document.getElementById("tb").appendChild(tr);
-}
+// for(let a=1; 11>a; a++){
+//     var tr = document.createElement('tr');
+//     var td1 = tr.appendChild(document.createElement('td'));
+//     var td2 = tr.appendChild(document.createElement('td'));
+//     var td3 = tr.appendChild(document.createElement('td'));
+//     td1.innerHTML = x;
+//     td2.innerHTML = a;
+//     td3.innerHTML = a*x;
+//     document.getElementById("tb").appendChild(tr);
+// }
 
-for(let i = 1; i<=x; i++){
-    for(let j = 1; j<=i; j++){
-        document.write(j);
-    }
-    document.write("<br>")
-}
+// for(let i = 1; i<=x; i++){
+//     for(let j = 1; j<=i; j++){
+//         document.write(j);
+//     }
+//     document.write("<br>")
+// }
+
+var fruits = ["apple","mango","banana"];
+fruits.forEach(function(value){
+    document.write(value + "<br>")
+});
+// document.write(fruits[1]);
